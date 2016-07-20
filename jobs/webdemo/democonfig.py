399790d45a829ecd4fcbd21069b9143ebddf98a3
Tom Cardozo <tom.cardozo@gmail.com>
2016-07-20 13:49:44 -0400

    cleanup

--- jobs/webdemo/democonfig.py
+++ jobs/webdemo/democonfig.py
@@ -12,5 +12,5 @@ APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/jobs.db'.format(APPLICATION_DIR)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-print('from app: ', APPLICATION_DIR)
-print('from app: ', SQLALCHEMY_DATABASE_URI)
+# print('from app: ', APPLICATION_DIR)
+# print('from app: ', SQLALCHEMY_DATABASE_URI)
